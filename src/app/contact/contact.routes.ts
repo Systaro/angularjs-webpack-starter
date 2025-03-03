703f88a142fb92222a21453de7e191248010882f
Antonia Engfors <antonia@systaro.de>
2025-03-03 12:20:22 +0100

    cleanup some routing and styling

--- src/app/contact/contact.routes.ts
+++ src/app/contact/contact.routes.ts
@@ -10,16 +10,17 @@ export default contactRoutingConfig.name;
 Config.$inject = ['$stateProvider'];
 function Config($stateProvider){
     $stateProvider
-      .state('add-contact', {
-        url: '/contacts/add',
-        template: require('./contact-form/contact-form.controller.html'),
-        controller: 'contactFormController as vm'
-      })
-
       .state('contacts', {
         url: '/contacts',
         template: require('./contact-list/contact-list.controller.html'),
         controller: 'contactListController as vm'
+      })
+
+      .state('add-contact', {
+        url: '/contacts/add',
+        template: require('./contact-form/contact-form.controller.html'),
+        controller: 'contactFormController as vm'
       });
+
 }
 
